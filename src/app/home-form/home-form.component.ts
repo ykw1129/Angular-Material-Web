41db9675122cfa802a37ab0b963e4c79511bd54d
killian <1794093413@qq.com>
2021-05-27 16:56:26 +0800

    测试了下响应式表单和驱动表单的差别

--- src/app/home-form/home-form.component.ts
+++ src/app/home-form/home-form.component.ts
@@ -1,35 +1,44 @@
 import { Component, Injectable, OnInit } from '@angular/core';
-import { FormControl,FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-home-form',
   templateUrl: './home-form.component.html',
   styleUrls: ['./home-form.component.less'],
 })
-export class HomeFormComponent implements OnInit{
-  myForm = new FormGroup({
-    name : new FormControl(),
-    sex : new FormControl('Man'),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
-    })
-  })
-
+export class HomeFormComponent implements OnInit {
+  public myForm!: FormGroup;
+  public user = { name: '', sex: 'Man' }
+  public model = new Person('','Man')
   constructor() {
 
-   }
-
-  ngOnInit(){
-
-  }
-  ngDoCheck(){
-    console.log('ngDoCheck')
-  }
-  onSubmit(){
-    console.table(this.myForm.value)
   }
 
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      name: new FormControl(this.user.name, [
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+      sex: new FormControl(this.user.sex, [
+        Validators.required,
+        Validators.minLength(1)
+      ])
+    })
+  }
+  get form() {
+    return this.myForm.controls
+  }
+  ngDoCheck() {
+    console.log(this.model)
+  }
+  onSubmit() {
+    console.table(this.myForm.valid)
+  }
+
+}
+class Person {
+  constructor(public name: string, public sex: string) {
+
+  }
 }
